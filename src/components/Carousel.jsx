459cc315904584c8edd97a2a0b36c470f335dcdf
verld <soe.elamus@gmail.com>
2026-01-08 20:34:00 +0100

    V0.220: Improved about us text

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 
 const Carousel = () => (
-      <section id="1" className="container-carousel">
+      <><section id="1" className="container-carousel">
       <div className="container-carousel-header">
         <header className="major">
-          <h2>Welkom bij Aliina's – Pizza in Oost-Vlaanderen!</h2>
+          <h2>Aliina's Pizza - Versgebakken pizza’s voor jouw feestje!</h2>
         </header>
+        <p>Wij houden van feestjes en willen graag bij jou langskomen! 🎉 Aliina's heeft een aantal vaste staanplaatsen waar je alvast kan komen proeven. Neem een kijkje op onze kalender en kom gezellig langs.</p>
+        <p><a href="#kalender" className="btn-purple scrolly">
+              Kalender
+            </a></p>
         <p>
-          Versgebakken pizza’s met karakter, rijke smaken en toppings die je
-          smaakpapillen laten dansen – onze foodtruck
-          is de plek waar echte pizzadromen tot leven komen.
-          <br/><br/>
           We bereiden ons deeg elke dag opnieuw en werken uitsluitend met verse
           groenten, kazen en kwaliteitsvol vlees.
-          Niets wordt ooit ingevroren, zodat elke hap de pure, volle versheid
-          uit onze keuken draagt.
+          We gebruiken geen diepvriezer, zodat elke hap de volle versheid
+          uit onze keuken draagt. 🍕
         </p>
       </div>
       <div className="slideshow-container" id="slideshow">
        <img loading="lazy" src="images/Mood.png" alt="Friends sitting around a table sharing a fresh Neapolitan pizza"/>
         </div>
       </section>
+      <br id="kalender"/><br /><br />
+      </>
 );
 
 export default Carousel;
