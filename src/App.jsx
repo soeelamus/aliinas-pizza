e169becceca7f7de00290801f55472fdc2a24973
verld <soe.elamus@gmail.com>
2026-01-08 12:36:23 +0100

    V0.210: Added successpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Import Compontents
+// Import components
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Carousel from "./components/Carousel";
@@ -9,27 +10,36 @@ import HowWork from "./components/HowWork";
 import ContactForm from "./components/ContactForm";
 import PizzaShop from './components/PizzaShop';
 import Cart from "./components/Cart";
-
-// Import CSS
 import Footer from "./components/Footer";
+import SuccessPage from "./components/SuccessPage"; // nieuwe component
+
+// CSS
 import "./assets/css/main.css";
 import "leaflet/dist/leaflet.css";
 
-
 function App() {
-    return (
-    <div className="App">
-      <Navbar />
-      <Header />
-      <Carousel />
-      <Calendar />
-      <HowWork />
-      <ContactForm />
-      <PizzaShop />
-      <Cart />
-      <Footer />
-    </div>
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Header />
+              <Carousel />
+              <Calendar />
+              <HowWork />
+              <ContactForm />
+              <PizzaShop />
+              <Cart />
+            </>
+          } />
+          <Route path="/success" element={<SuccessPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
-export default App
+export default App;
---
+++ src/components/SuccesPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SuccessPage = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🎉 Bedankt voor je bestelling!</h1>
+      <p>Je betaling is geslaagd. We gaan meteen aan de slag met je take-out bestelling.</p>
+    </div>
+  );
+};
+
+export default SuccessPage;
