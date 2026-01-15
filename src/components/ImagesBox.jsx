dd836ff069d730483cdc298423eb4c0a55ea6ecd
verld <soe.elamus@gmail.com>
2026-01-15 21:21:39 +0100

    Added new pizza image

--- src/components/ImagesBox.jsx
+++ src/components/ImagesBox.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const ImagesBox = () => (
       <section className="imagesbox">
             <img className="imagesbox-img" loading="lazy" src="images/Mood.png" alt="Friends sitting around a table sharing a fresh Neapolitan pizza"/>
+            <img className="imagesbox-img" loading="lazy" src="images/image-pizza.png" alt="Pizza on a plate, next to a lovely dessert"/>
             <img className="imagesbox-img" loading="lazy" src="images/Mood.png" alt="Friends sitting around a table sharing a fresh Neapolitan pizza"/>
-            <img className="imagesbox-img" loading="lazy" src="images/Mood.png" alt="Friends sitting around a table sharing a fresh Neapolitan pizza"/>
-            <img className="imagesbox-img" loading="lazy" src="images/Mood.png" alt="Friends sitting around a table sharing a fresh Neapolitan pizza"/>
+            <img className="imagesbox-img" loading="lazy" src="images/image-pizza.png" alt="Pizza on a plate, next to a lovely dessert"/>
       </section>
 );
 
