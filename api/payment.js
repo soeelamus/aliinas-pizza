33252a33a9d9650a98008eca3b3b9fbf859b165b
verld <soe.elamus@gmail.com>
2026-01-29 22:44:32 +0100

    V0.516: Removed hardcoded accepted payments

--- api/payment.js
+++ api/payment.js
@@ -38,7 +38,6 @@ export default async function handler(req, res) {
 
       // Maak Stripe Checkout Session
       const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card", "ideal"], // iDEAL + kaart
         mode: "payment",
 
         line_items: [
