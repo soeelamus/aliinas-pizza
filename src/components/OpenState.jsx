05053da5ba17f285ee698b2735878c0147b82114
verld <soe.elamus@gmail.com>
2026-02-19 21:02:35 +0100

    V1.232: Improved user feedback on Pizzashop IsOpen: false

--- src/components/OpenState.jsx
+++ src/components/OpenState.jsx
@@ -11,25 +11,29 @@ const OpenState = ({ isOpen, events, onRoute }) => {
   return (
     <div className="menu-openStatus" style={{ fontWeight: "bold" }}>
       {isOpen ? (
-        <div className="center">
+        <div className="center background--purple">
           <h3 className="menu-openStatus">
-            We zijn vandaag geopend van {todayEvent.startTime} tot {todayEvent.endTime} 
+            We zijn vandaag geopend van {todayEvent.startTime} tot{" "}
+            {todayEvent.endTime}
           </h3>
           {!onRoute && (
             <>
-            <a href="/ordering" className="btn-purple btn-order">
-              ✅ Bestellen
-            </a>
-            <a href="/ordering" className="btn-purple cta-btn">
-              ✅ Bestellen
-            </a>
+              <a href="/ordering" className="btn-purple btn-order">
+                ✅ Bestellen
+              </a>
+              <a href="/ordering" className="btn-purple cta-btn">
+                ✅ Bestellen
+              </a>
             </>
           )}
           <br />
           <p className="menu-openStatus-p">Ophalen: {todayEvent.address}</p>
         </div>
       ) : (
-        <p className="menu-openStatus-p">❌ We zijn vandaag gesloten</p>
+        <div className="center background--purple">
+          <p className="menu-openStatus-p">❌ We zijn vandaag gesloten</p>
+          <p className="menu-openStatus-p">Online bestellen is enkel mogelijk tijdens onze openingsuren</p>
+        </div>
       )}
     </div>
   );
