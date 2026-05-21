b0801b296c012212157ba4127edaeecde3cabe18
verld <soe.elamus@gmail.com>
2026-05-21 03:11:00 +0200

    V1.608: Added dynamic AD

--- src/components/AdBox.jsx
+++ src/components/AdBox.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const AdBox = () => {
+  const [i, setI] = useState(1);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setLoaded(false);
+
+    const interval = setInterval(() => {
+      setI((prev) => (prev % 3) + 1);
+    }, 1500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const src = `/images/ads/combo_ad${i}.png`;
+
   return (
     <div className="adbox">
+      {!loaded && <div className="adbox--placeholder" />}
+
       <a href="#ad" className="adbox--img-box">
         <img
           className="adbox--img"
-          src="/images/combo_ad.png"
+          src={src}
           alt="Combo Deal"
+          onLoad={() => setLoaded(true)}
+          style={{ display: loaded ? "block" : "none" }}
         />
       </a>
     </div>
