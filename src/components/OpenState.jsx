ee3764e0b69df006b43426e609496f90ba261291
verld <soe.elamus@gmail.com>
2026-08-08 00:45:34 +0200

    V1.807: Improved user feedback for OPENSTATE

--- src/components/OpenState.jsx
+++ src/components/OpenState.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
 const OpenState = ({ isOpen, events }) => {
+  if (events.length === 0) {
+    return;
+  }
+
   const today = new Date().toLocaleDateString("en-CA");
 
-  const todayEvent = events?.find(
+  const todayEvent = events.find(
     (e) => e.type.toLowerCase() !== "privaat" && e.date === today,
   );
+
+  const now = new Date();
+
+  const endTime = todayEvent
+    ? new Date(`${today}T${todayEvent.endTime}`)
+    : null;
+
+  const isPastClosingTime = endTime && now >= endTime;
+
   localStorage.setItem("location", JSON.stringify(todayEvent));
 
   return (
@@ -13,18 +26,35 @@ const OpenState = ({ isOpen, events }) => {
       {isOpen ? (
         <div className="center background--purple">
           <h3 className="menu-openStatus">
-            We zijn vandaag geopend van {todayEvent.startTime} tot{" "}
+            🟢 We zijn vandaag geopend van {todayEvent.startTime} tot{" "}
             {todayEvent.endTime}
           </h3>
+
           <a href="#ad" className="btn-purple btn-order">
             ✅ Bestellen
           </a>
+
           <br />
+
           <p className="menu-openStatus-p">Ophalen: {todayEvent.address}</p>
+        </div>
+      ) : todayEvent && !isPastClosingTime ? (
+        <div className="center background--purple">
+          <p className="menu-openStatus-p">
+            🟠 Online bestellen kan niet meer.{" "}
+            <p>Kom gerust langs tot {todayEvent.endTime}.</p>
+          </p>
+        </div>
+      ) : todayEvent && isPastClosingTime ? (
+        <div className="center background--purple">
+          <p className="menu-openStatus-p">
+            🔴 Online bestellen kan niet meer. We zijn reeds gesloten.
+          </p>
         </div>
       ) : (
         <div className="center background--purple">
-          <p className="menu-openStatus-p">❌ We zijn vandaag gesloten</p>
+          <p className="menu-openStatus-p">🔴 We zijn vandaag gesloten</p>
+
           <p className="menu-openStatus-p">
             Online bestellen is enkel mogelijk op de dag van afhaal
           </p>
